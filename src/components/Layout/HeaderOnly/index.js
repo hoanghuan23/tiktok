a476b7804f92a23d283c0f0509ff50458969cddd
hoanghuan23 <huanhoang2003@gmail.com>
2026-01-15 10:38:38 +0700

    Cấu hình Layout/Router

---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,12 @@
+
+function DefaultLayout({children}) {
+    return (
+        <div>
+            <div className="container">
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default DefaultLayout;
