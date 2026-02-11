c8dfbe67619b6cbf8a29a2d9b8def6aa81b1b01a
hoanghuan23 <huanhoang2003@gmail.com>
2026-02-11 11:28:19 +0700

    done button Login

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -55,7 +55,7 @@ function Header() {
         </Tippy>
         <div className={cx('actions')}>
           <Button text>Upload</Button>
-          <Button primary rightIcon={<FontAwesomeIcon icon={faSignIn}></FontAwesomeIcon>}>Login</Button>
+          <Button primary>Log in</Button>
         </div>
       </div>
     </header>
