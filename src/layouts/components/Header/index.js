d5dcf4584d8b9fe77e7f987cc49267d18135d344
hoanghuan23 <huanhoang2003@gmail.com>
2026-03-19 22:58:40 +0700

    tái cấu trúc code

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -23,7 +23,7 @@ import Menu from '~/components/Popper/Menu';
 import styles from './Header.moudle.scss';
 
 import Search from '../Search';
-import routesConfig from '~/config/routes';
+import config from '~/config';
 
 const cx = classNames.bind(styles);
 const MENU_ITEMS = [
@@ -106,7 +106,7 @@ function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        <Link to= {routesConfig.home} className={cx('logo-link')}>
+        <Link to= {config.routes.home} className={cx('logo-link')}>
           <img src={images.logo.default} alt="Logo" />
         </Link>
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,17 @@
-import { HeaderOnly } from '~/components/Layout';
+import { HeaderOnly } from '~/layouts';
 import Home from '~/pages/Home';
 
-import routesConfig from '~/config/routes';
+import config from '~/config';
 
 import Following from '~/pages/Following';
 import Upload from '~/pages/Upload';
 import Profile from '~/pages/Profile';
 
 const publicRoutes = [
-  { path: routesConfig.home, component: Home },
-  { path: routesConfig.following, component: Following },
-  { path: routesConfig.profile, component: Profile },
-  { path: routesConfig.upload, component: Upload, layout: HeaderOnly },
+  { path: config.routes.home, component: Home },
+  { path: config.routes.following, component: Following },
+  { path: config.routes.profile, component: Profile },
+  { path: config.routes.upload, component: Upload, layout: HeaderOnly },
 ];
 
 const privateRoutes = [];
